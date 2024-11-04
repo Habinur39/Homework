5f77f9509902c1e9b990befb159167fc5b5e9a24
Habinur39 <lapki.detok72@gmail.com>
2024-11-04 17:40:57 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+hours_spent = 1.5
+course_name ='Python'
+time_per_task = hours_spent / completed_homework
+print('Курс:',course_name + ', Всего задач:', (str(completed_homework)) + ', Затрачено часов:', (str(hours_spent)) + ', Cреднее время выполнения задания:' ,(str(time_per_task)) + '.')
